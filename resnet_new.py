8cc0ddd8cb78861ff44b924728ed72e344694a8f
Shaumik-Ashraf <a.shaumik@gmail.com>
2021-12-08 18:02:30 -0500

    create train_test_split script for new dataset

---
+++ resnet_new.py
@@ -0,0 +1,181 @@
+# train and run resnet on CXR images and guess continuous CTR
+
+import os;
+import glob;
+import time;
+import torch;
+import argparse;
+from PIL import Image;
+from torch.utils.data import Dataset;
+from torch.utils.data import DataLoader;
+from torch import nn;
+from torch.nn.functional import mse_loss as mean_square_error;
+from torchvision import models;
+#from torchvision.datasets import MNIST;
+from torchvision import transforms;
+#from torchvision.io import write_png;
+#from sklearn.metrics import accuracy_score;
+from matplotlib import pyplot as plt;
+
+
+BATCH_SIZE = 4;
+WORKERS = 4;
+LEARNING_RATE = 0.0001;
+EPOCHS = 5;
+
+
+TRANSFORM = transforms.Compose([
+    transforms.ToTensor(),
+    # transforms.Grayscale() # PIL reads as grayscale
+    transforms.ConvertImageDtype(torch.float),
+    # Normalization?
+]);
+
+
+parser = argparse.ArgumentParser(description="Train and test RESNET18 on CXRs to predict CTR (with MSE loss and Adam optimizer).");
+parser.add_argument("--test-only", action="store_true", help="skip training, load model, and test");
+
+
+class ToFloat():
+    def __init__(self):
+        self.target_dtype = torch.float
+
+    def __call__(self, scalar):
+        return torch.tensor(scalar, dtype=self.target_dtype);
+
+class CTRData(Dataset):
+    def __init__(self, cxr_dir, ctr_file, transform = None, target_transform = None):
+        super(CTRData, self).__init__();
+        self.imgs = glob.glob(os.path.join(cxr_dir, "*"));
+
+        f = open(ctr_file, "r");
+        lines = f.read().split("\n");
+        f.close();
+
+        self.ctr_map = {};
+        for line in lines:
+            tokens = line.split();
+            if len(tokens) > 0:
+                self.ctr_map[tokens[0]] = float(tokens[2]);
+
+        self.transform = transform;
+        self.target_transform = target_transform;
+
+    def __getitem__(self, i):
+        x = Image.open(self.imgs[i]);
+        y = self.ctr_map[ os.path.basename(self.imgs[i]) ];
+
+        if self.transform:
+            x = self.transform(x);
+
+        if self.target_transform:
+            y = self.target_transform(y);
+
+        return x, y
+
+    def __len__(self):
+        return len(self.imgs);
+
+    def debug(self):
+        print(self.imgs);
+        print(self.ctr_map);
+
+
+class Resnet(nn.Module):
+    "Resnet18 adapted for CXR and CTR prediction"
+
+    def __init__(self):
+        super(Resnet, self).__init__();
+        self.resnet18 = models.resnet18(); # random weights
+        self.resnet18.conv1 = nn.Conv2d(1, 64, 7, stride = 2, padding = 3); # first layer, grayscale input
+        self.resnet18.fc = nn.Linear(512, 1, bias=True); # last computation layer
+        self.flatten = nn.Flatten(0); # make output scalar (1D array for batch)
+
+    def forward(self, x):
+        return self.flatten( self.resnet18(x) );
+
+
+def train(loader, model, loss_func, optim, epochs):
+    steps = len(loader);
+    model.train();
+
+    for epoch in range(epochs):
+        for i, (x_batch, y_batch) in enumerate(loader):
+
+            #print(x_batch);
+            #print("---");
+            #print(y_batch);
+
+            y_pred = model(x_batch);
+
+            #print("---");
+            #print(y_pred);
+
+            loss = loss_func(y_pred, y_batch);
+
+            optim.zero_grad();
+            loss.backward();
+            optim.step();
+
+            if( True or (i+1)%100 == 0 ): # print everything atm
+                print(f"Epoch {epoch+1}/{epochs}, Step {i+1}/{steps}, Loss {loss.item()}");
+
+def test(loader, model):
+    model.eval();
+    scores = [];
+
+    with torch.no_grad():
+        #correct = 0;
+        #total = 0;
+
+        for images, y_batch in loader:
+            y_pred = model(images);
+            mse = mean_square_error(y_batch, y_pred);
+            scores.append(mse);
+
+    print(f"Micro-averaged Mean Squared Error {sum(scores)/len(scores)}")
+
+
+if __name__ == "__main__":
+
+    args = parser.parse_args();
+
+
+    print("===");
+    print("Loading & Initializing Data");
+    print("===");
+
+    train_cxr_folder = os.path.join('data', 'new', 'train_imgs');
+    #test_cxr_folder = os.path.join('data', 'split',  'preprocessed', 'test');
+    labels_file = os.path.join('data', 'CTR_Logs.txt');
+
+    train_set = CTRData(train_cxr_folder, labels_file, transform=TRANSFORM, target_transform=ToFloat());
+    test_set = CTRData(test_cxr_folder, labels_file, transform=TRANSFORM, target_transform=ToFloat());
+
+    train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS);
+    test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS);
+
+
+    resnet = Resnet();
+    mse_loss = nn.MSELoss();
+    adam = torch.optim.Adam(resnet.parameters(), lr = LEARNING_RATE);
+
+    print(resnet);
+
+    if not args.test_only:
+        print("===");
+        print("Training Model");
+        print("===");
+        start = time.time();
+        train(train_loader, resnet, mse_loss, adam, EPOCHS);
+        print(f"Finished in {(time.time() - start) / 60} minutes");
+        torch.save(resnet.state_dict(), "resnet.pt"); # give better name?
+
+    print("===");
+    print("Testing");
+    print("===");
+
+    if args.test_only:
+        resnet.load_state_dict(torch.load("resnet.pt"));
+
+    test(test_loader, resnet);
--- train_test_split.py
+++ train_test_split.py
@@ -13,7 +13,6 @@ from sklearn.model_selection import train_test_split;
 # splits should add upto 1
 # if we do cross validation, set validate fraction to 0
 TRAIN_FRACTION = 0.8
-VALIDATE_FRACTION = 0
 TEST_FRACTION = 0.2
 
 INPUT_DIR = os.path.join("data", "new", "imgs");
@@ -26,14 +25,12 @@ imgs = glob.glob(os.path.join(INPUT_DIR, "*"));
 masks = glob.glob(os.path.join(INPUT_MASKS_DIR, "*"));
 
 train_prefix = os.path.join(OUTPUT_DIR, "train");
-validate_prefix = os.path.join(OUTPUT_DIR, "validate");
 test_prefix = os.path.join(OUTPUT_DIR, "test");
 
 train_masks_prefix = os.path.join(OUTPUT_MASKS_DIR, "train");
-validate_masks_prefix = os.path.join(OUTPUT_MASKS_DIR, "validate");
 test_masks_prefix = os.path.join(OUTPUT_MASKS_DIR, "test");
 
-x_dev, x_test, y_dev, y_test = train_test_split(imgs, masks, test_size=TEST_FRACTION);
+x_train, x_test, y_train, y_test = train_test_split(imgs, masks, test_size=TEST_FRACTION);
 #print(imgs[0]);
 #print(len(imgs));
 #print(masks[0]);
@@ -47,26 +44,25 @@ print("test set x-rays:", len(x_test));
 #print(y_test[0]);
 print("test set masks:", len(y_test));
 
-x_train, x_val, y_train, y_val = train_test_split(x_dev, y_dev, test_size=(1-TEST_FRACTION)*VALIDATE_FRACTION);
 #print(x_train[0]);
 print("train set x-rays:", len(x_train));
 #print(y_train[0]);
 print("train set masks:", len(y_train));
 #print(x_val[0]);
-print("validation set x-rays:", len(x_val));
+#print("validation set x-rays:", len(x_val));
 #print(y_val[0]);
-print("validation set masks:", len(y_val));
+#print("validation set masks:", len(y_val));
 
 
 # generate dirs if they don't exist
-for dir in [train_prefix, validate_prefix, test_prefix, train_masks_prefix, validate_masks_prefix, test_masks_prefix]:
+for dir in [train_prefix, test_prefix, train_masks_prefix, test_masks_prefix]:
     os.makedirs(dir, exist_ok = True);
 
 # move files by split
 for set, prefix in [(x_train, train_prefix),
                     (y_train, train_masks_prefix),
-                    (x_val, validate_prefix),
-                    (y_val, validate_masks_prefix),
+                    #(x_val, validate_prefix),
+                    #(y_val, validate_masks_prefix),
                     (x_test, test_prefix),
                     (y_test, test_masks_prefix)
                    ]:
@@ -74,4 +70,3 @@ for set, prefix in [(x_train, train_prefix),
     filename = os.path.basename(path);
     new_path = os.path.join(prefix, filename);
     shutil.copy(path, new_path);
-
